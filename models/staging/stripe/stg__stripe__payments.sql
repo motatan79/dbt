733041e536fa64e262c2bd3b08bfbbe075ca4d15
Moises Pirela <m.pirela@gmail.com>
2026-05-22 18:49:27 +0000

    updated staging files

--- models/staging/stripe/stg__stripe__payments.sql
+++ models/staging/stripe/stg__stripe__payments.sql
@@ -2,7 +2,7 @@ SELECT
     orderid as order_id
     ,paymentmethod as payment_method
     ,status as payment_status
-    ,amount as payment_amount
+    ,amount/100 as payment_amount
     ,created
     ,_batched_at
 FROM {{ source('stripe', 'payment') }}

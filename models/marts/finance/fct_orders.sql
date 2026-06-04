249d86fc0db6b9705c07804143585fdeb93660e8
motatan <m.pirela@gmail.com>
2026-06-04 05:48:31 -0300

    Added incremental

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,3 +1,10 @@
+{{
+    config(
+            materialized = 'incremental'
+    )
+}}
+
+
 with orders as  (
     select * from {{ ref ('stg__jaffle_shop__orders' )}}
 ),
@@ -28,3 +35,7 @@ order_payments as (
 )
 
 select * from final
+{% if is_incremental() %}
+    where order_date >= (select max(order_date) from {{ this }})
+{% endif %}
+order by order_date desc

9262b824e4713098a077d2ac396979fa13b80168
motatan <m.pirela@gmail.com>
2026-06-08 17:26:10 -0300

    tests

---
+++ tests/assert_amount_average_is_greater_than_one.sql
@@ -0,0 +1,10 @@
+{% test assert_amount_is_greater_than_one( model, column_name, group_by_column) %}
+
+    SELECT 
+        {{ group_by_column }}
+        ,avg( {{ column_name }}) as average_amount
+    FROM {{ model }}
+    GROUP BY 1
+    HAVING average_amount < 1
+
+{ endtest }
